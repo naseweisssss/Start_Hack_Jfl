f84135c3e7de84856ad84e68bca229e203bed2e8
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2023-03-23 10:54:52 +0100

    added google maps

---
+++ jfl-solar/src/app/maptest/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Loader, LoaderOptions } from "google-maps";
+import { useEffect, useLayoutEffect, useRef } from "react";
+
+// `app/page.js` is the UI for the root `/` URL
+export default function maptest() {
+
+  return <div style={{ height: "400px" }}></div>;
+}
