fb5e26b52e41f702244c21e2050af6062a5bf4ce
josh <114422375+runningtorpedo@users.noreply.github.com>
2023-03-23 19:25:35 +0000

    dynamically fetch data

--- jfl-solar/src/app/maptest/mapbox.tsx
+++ jfl-solar/src/app/maptest/mapbox.tsx
@@ -1,89 +1,83 @@
 "use client";
 
 import { Loader, LoaderOptions } from "google-maps";
-import { CSSProperties, Dispatch, SetStateAction, useEffect, useLayoutEffect, useRef } from "react";
+import {
+  CSSProperties,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+} from "react";
 import { GoogleMap, LoadScript, KmlLayer } from "@react-google-maps/api";
 
 const containerStyle: CSSProperties = {
-    width: "100%",
-    height: "600px",   
-    boxSizing: `borderBox`,
-} ;
+  width: "100%",
+  height: "600px",
+  boxSizing: `borderBox`,
+};
 
 let center = {
-    lat: 46,
-    lng: 7.64,
+  lat: 46,
+  lng: 7.64,
 };
 
 interface mapProps {
-    setter: Dispatch<SetStateAction<{
-        lat: null;
-        lng: null;
-    }>>;
-    loading: boolean
+  setter: Dispatch<
+    SetStateAction<{
+      lat: null;
+      lng: null;
+    }>
+  >;
+  loading: boolean;
 }
 
-const MapBox: React.FC<mapProps> = ({
-    setter,
-    loading
-}) => {
+const MapBox: React.FC<mapProps> = ({ setter, loading }) => {
+  let pos: {
+    lat: number;
+    lng: number;
+  };
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // console.log("geolocation call")
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          console.log(pos);
 
-    let pos: {
-        lat: number;
-        lng: number;
+          center.lat = pos.lat;
+          center.lng = pos.lng;
+        },
+        () => {}
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      // handleLocationError(false, infoWindow, map.getCenter()!);
     }
-    useEffect(()=>{
-        if (navigator.geolocation) {
-            // console.log("geolocation call")
-            navigator.geolocation.getCurrentPosition(
-              (position: GeolocationPosition) => {
-                pos = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude,
-                };
-                console.log(pos)
+  }, [navigator.geolocation]);
 
-                center.lat = pos.lat;
-                center.lng = pos.lng;
-      
-              },
-              () => {
-                
-              }
-            );
-          } else {
-            // Browser doesn't support Geolocation
-            // handleLocationError(false, infoWindow, map.getCenter()!);
-          }
-      
-    }, [navigator.geolocation]);
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_API!}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onClick={(_) => {
+          let coordC = JSON.parse(JSON.stringify(_.latLng));
+          console.log(JSON.parse(JSON.stringify(_.latLng)));
+          setter({
+            lat: coordC.lat,
+            lng: coordC.lng,
+          });
+        }}
+      >
+        {/* <KmlLayer url="https://github.com/naseweisssss/Start_Hack_Jfl/raw/main/jfl-solar/public/World_PVOUT_GoogleEarthData_GlobalSolarAtlas.kmz"></KmlLayer> */}
+      </GoogleMap>
+    </LoadScript>
+  );
+};
 
-    return (
-   
-         <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_API!}>
-             <GoogleMap
-                 mapContainerStyle={containerStyle}
-                 center={center}
-                 zoom={10}
-                 onClick={(_)=>{
-                    let coordC = JSON.parse(JSON.stringify(_.latLng));
-                    console.log(JSON.parse(JSON.stringify(_.latLng)));
-                    if(!loading){
-                        setter({
-                            lat: coordC.lat,
-                            lng: coordC.lng
-                        });
-                    }
-                    
-                }}>
-                {/* <KmlLayer url="https://github.com/naseweisssss/Start_Hack_Jfl/raw/main/jfl-solar/public/World_PVOUT_GoogleEarthData_GlobalSolarAtlas.kmz"></KmlLayer> */}
-             
-               
-             </GoogleMap>
-         </LoadScript>
-
-
-    );
-}
-
-export default MapBox
+export default MapBox;
--- jfl-solar/src/app/page.tsx
+++ jfl-solar/src/app/page.tsx
@@ -5,20 +5,20 @@ import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import TutorialHeader from "./headerShell";
 import {
-    Content,
-    NumberInput,
-    Tile,
-    Column,
-    Grid,
-    Row,
-    Form,
-    Button,
-    StructuredListWrapper,
-    StructuredListHead,
-    StructuredListBody,
-    StructuredListCell,
-    StructuredListRow,
-    FormGroup,
+  Content,
+  NumberInput,
+  Tile,
+  Column,
+  Grid,
+  Row,
+  Form,
+  Button,
+  StructuredListWrapper,
+  StructuredListHead,
+  StructuredListBody,
+  StructuredListCell,
+  StructuredListRow,
+  FormGroup,
 } from "carbon-components-react";
 import MapBox from "./maptest/mapbox";
 
@@ -28,127 +28,107 @@ import { useEffect, useRef, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const [coord, setCoord] = useState({ lat: null, lng: null });
-    const [area, setArea] = useState(1);
-    const [coverage, setCoverage] = useState(85);
-    const [loading, setLoading] = useState(false);
-    const numberRef = useRef();
-    const pctRef = useRef();
+  const [coord, setCoord] = useState({ lat: null, lng: null });
+  const [area, setArea] = useState(1);
+  const [coverage, setCoverage] = useState(85);
+  const [loading, setLoading] = useState(false);
+  const numberRef = useRef();
+  const pctRef = useRef();
+  const [pva, setPVA] = useState(null);
 
-    const fetchSolar = async () => {
-        console.log("fetch")
-        setLoading(true)
-        fetch("https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=ALLSKY_SFC_SW_DWN&community=RE&longitude=-104.1862&latitude=31.3468&format=json&start=2021&end=2021").then(
-            (res)=>res.json())
-        .then((data)=>{
-            console.log(data.properties.parameter.ALLSKY_SFC_SW_DWN['202113'])
-        })
-    }
+  const fetchSolar = async () => {
+    console.log("fetch", coord?.lat, coord?.lng);
+    setLoading(true);
+    fetch(
+      `https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=ALLSKY_SFC_SW_DWN&community=RE&longitude=${coord?.lng}&latitude=${coord?.lat}&format=json&start=2021&end=2021`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.properties.parameter.ALLSKY_SFC_SW_DWN["202113"]);
+      });
+  };
 
-    useEffect(()=>{
-        fetchSolar();
-    }, [coord])
-    
-    return (
-        <main>
-            <TutorialHeader></TutorialHeader>
-            <Content>
-                <Grid>
-                    <Column lg={12}>
-                        <Row>
-                            <MapBox setter={setCoord} loading={loading}></MapBox>
-                        </Row>
-                    </Column>
-                    <Column lg={4}>
-                        <Row>
-                            <Grid>
-                                <Column lg={4}>
-                                    <Row>
-                                        <h3>Input Parameters:</h3>
-                                        </Row>
-                                    <Row>
-                                        <FormGroup>
-                                            <NumberInput
-                                                ref={numberRef}
-                                                id={"jhgfjhg"}
-                                                value={0}
-                                                hideSteppers={true}
-                                                label={"Roof Area (m^2)"}
-                                                onKeyUp={() => {
-                                                    // console.log(parseInt(numberRef.current.value))
-                                                    setArea(
-                                                        parseInt(
-                                                            numberRef.current
-                                                                .value
-                                                        )
-                                                    );
-                                                }}
-                                            ></NumberInput>
-                                            <NumberInput
-                                                ref={pctRef}
-                                                id={"jhgfjhga"}
-                                                value={85}
-                                                hideSteppers={true}
-                                                label={"Percentage Coverage"}
-                                                onKeyUp={() => {
-                                                    // console.log(parseInt(numberRef.current.value))
-                                                    setArea(
-                                                        parseInt(
-                                                            pctRef.current
-                                                                .value
-                                                        )
-                                                    );
-                                                }}
-                                            ></NumberInput>
-                                        </FormGroup>
-                                    </Row>
-                                    <Row  style={{marginTop: "1rem"}}>
-                                        <h3>
-                                            Results:
-                                        </h3>
-                                    </Row>
-                                    <Row>
-                                        <StructuredListWrapper>
-                                            <StructuredListHead>
-                                                <StructuredListRow head>
-                                                    <StructuredListCell head>
-                                                        ColumnA
-                                                    </StructuredListCell>
-                                                    <StructuredListCell head>
-                                                        ColumnB
-                                                    </StructuredListCell>
-                                                    
-                                                </StructuredListRow>
-                                            </StructuredListHead>
-                                            <StructuredListBody>
-                                                <StructuredListRow>
-                                                    <StructuredListCell noWrap>
-                                                        Row 1
-                                                    </StructuredListCell>
-                                                    <StructuredListCell>
-                                                        Row 1
-                                                    </StructuredListCell>
-                                                    
-                                                </StructuredListRow>
-                                                <StructuredListRow>
-                                                    <StructuredListCell noWrap>
-                                                        Row 2
-                                                    </StructuredListCell>
-                                                    <StructuredListCell>
-                                                        Row 2
-                                                    </StructuredListCell>
-                                                    
-                                                </StructuredListRow>
-                                            </StructuredListBody>
-                                        </StructuredListWrapper>
-                                    </Row>
-                                </Column>
-                            </Grid>
-                        </Row>
-                    </Column>
-                </Grid>
-            </Content>
+  useEffect(() => {
+    fetchSolar();
+  }, [coord]);
 
-        </main>
-    );
+  return (
+    <main>
+      <TutorialHeader></TutorialHeader>
+      <Content>
+        <Grid>
+          <Column lg={12}>
+            <Row>
+              <MapBox setter={setCoord} loading={loading}></MapBox>
+            </Row>
+          </Column>
+          <Column lg={4}>
+            <Row>
+              <Grid>
+                <Column lg={4}>
+                  <Row>
+                    <h3>Input Parameters:</h3>
+                  </Row>
+                  <Row>
+                    <FormGroup>
+                      <NumberInput
+                        ref={numberRef}
+                        id={"jhgfjhg"}
+                        value={0}
+                        hideSteppers={true}
+                        label={"Roof Area (m^2)"}
+                        onKeyUp={() => {
+                          // console.log(parseInt(numberRef.current.value))
+                          setArea(parseInt(numberRef.current.value));
+                        }}
+                      ></NumberInput>
+                      <NumberInput
+                        ref={pctRef}
+                        id={"jhgfjhga"}
+                        value={85}
+                        hideSteppers={true}
+                        label={"Percentage Coverage"}
+                        onKeyUp={() => {
+                          // console.log(parseInt(numberRef.current.value))
+                          setArea(parseInt(pctRef.current.value));
+                        }}
+                      ></NumberInput>
+                    </FormGroup>
+                  </Row>
+                  <Row
+                    style={{ marginTop: "1rem" }}
+                    onClick={() => {
+                      console.log(coord);
+                    }}
+                  >
+                    <h3>Results:</h3>
+                  </Row>
+                  <Row>
+                    <StructuredListWrapper>
+                      <StructuredListHead>
+                        <StructuredListRow head>
+                          <StructuredListCell head>ColumnA</StructuredListCell>
+                          <StructuredListCell head>ColumnB</StructuredListCell>
+                        </StructuredListRow>
+                      </StructuredListHead>
+                      <StructuredListBody>
+                        <StructuredListRow>
+                          <StructuredListCell noWrap>Row 1</StructuredListCell>
+                          <StructuredListCell>Row 1</StructuredListCell>
+                        </StructuredListRow>
+                        <StructuredListRow>
+                          <StructuredListCell noWrap>Row 2</StructuredListCell>
+                          <StructuredListCell>Row 2</StructuredListCell>
+                        </StructuredListRow>
+                      </StructuredListBody>
+                    </StructuredListWrapper>
+                  </Row>
+                </Column>
+              </Grid>
+            </Row>
+          </Column>
+        </Grid>
+      </Content>
+    </main>
+  );
 }
