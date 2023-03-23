f867949dc452b8c2eeb95f5cd5fc4596cf0d67e8
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2023-03-23 19:54:23 +0100

    fetch

--- jfl-solar/src/app/maptest/mapbox.tsx
+++ jfl-solar/src/app/maptest/mapbox.tsx
@@ -6,11 +6,11 @@ import { GoogleMap, LoadScript, KmlLayer } from "@react-google-maps/api";
 
 const containerStyle: CSSProperties = {
     width: "100%",
-    height: "400px",   
+    height: "600px",   
     boxSizing: `borderBox`,
 } ;
 
-const center = {
+let center = {
     lat: 46,
     lng: 7.64,
 };
@@ -20,11 +20,43 @@ interface mapProps {
         lat: null;
         lng: null;
     }>>;
+    loading: boolean
 }
 
 const MapBox: React.FC<mapProps> = ({
-    setter
+    setter,
+    loading
 }) => {
+
+    let pos: {
+        lat: number;
+        lng: number;
+    }
+    useEffect(()=>{
+        if (navigator.geolocation) {
+            // console.log("geolocation call")
+            navigator.geolocation.getCurrentPosition(
+              (position: GeolocationPosition) => {
+                pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                console.log(pos)
+
+                center.lat = pos.lat;
+                center.lng = pos.lng;
+      
+              },
+              () => {
+                
+              }
+            );
+          } else {
+            // Browser doesn't support Geolocation
+            // handleLocationError(false, infoWindow, map.getCenter()!);
+          }
+      
+    }, [navigator.geolocation]);
 
     return (
    
@@ -35,11 +67,14 @@ const MapBox: React.FC<mapProps> = ({
                  zoom={10}
                  onClick={(_)=>{
                     let coordC = JSON.parse(JSON.stringify(_.latLng));
-                    console.log(JSON.parse(JSON.stringify(_.latLng)))
-                    setter({
-                        lat: coordC.lat,
-                        lng: coordC.lng
-                    });
+                    console.log(JSON.parse(JSON.stringify(_.latLng)));
+                    if(!loading){
+                        setter({
+                            lat: coordC.lat,
+                            lng: coordC.lng
+                        });
+                    }
+                    
                 }}>
                 {/* <KmlLayer url="https://github.com/naseweisssss/Start_Hack_Jfl/raw/main/jfl-solar/public/World_PVOUT_GoogleEarthData_GlobalSolarAtlas.kmz"></KmlLayer> */}
              
--- jfl-solar/src/app/page.tsx
+++ jfl-solar/src/app/page.tsx
@@ -4,18 +4,51 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import TutorialHeader from "./headerShell";
-import { Content, NumberInput, Tile, Column, Grid, Row, Form, Button } from "carbon-components-react";
+import {
+    Content,
+    NumberInput,
+    Tile,
+    Column,
+    Grid,
+    Row,
+    Form,
+    Button,
+    StructuredListWrapper,
+    StructuredListHead,
+    StructuredListBody,
+    StructuredListCell,
+    StructuredListRow,
+    FormGroup,
+} from "carbon-components-react";
 import MapBox from "./maptest/mapbox";
 
 import GTF from "./maptest/geotiff";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const [coord, setCoord] = useState({lat: null, lng: null});
-    const [input, setInput] = useState(1)
+    const [coord, setCoord] = useState({ lat: null, lng: null });
+    const [area, setArea] = useState(1);
+    const [coverage, setCoverage] = useState(85);
+    const [loading, setLoading] = useState(false);
     const numberRef = useRef();
+    const pctRef = useRef();
+
+    const fetchSolar = async () => {
+        console.log("fetch")
+        setLoading(true)
+        fetch("https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=ALLSKY_SFC_SW_DWN&community=RE&longitude=-104.1862&latitude=31.3468&format=json&start=2021&end=2021").then(
+            (res)=>res.json())
+        .then((data)=>{
+            console.log(data.properties.parameter.ALLSKY_SFC_SW_DWN['202113'])
+        })
+    }
+
+    useEffect(()=>{
+        fetchSolar();
+    }, [coord])
+    
     return (
         <main>
             <TutorialHeader></TutorialHeader>
@@ -23,27 +56,99 @@ export default function Home() {
                 <Grid>
                     <Column lg={12}>
                         <Row>
-                            <MapBox setter = {setCoord}></MapBox>
+                            <MapBox setter={setCoord} loading={loading}></MapBox>
                         </Row>
                     </Column>
-                    <Column lg = {4}>
+                    <Column lg={4}>
                         <Row>
-                            
-                            <Form>
-                                <NumberInput ref={numberRef} id={"jhgfjhg"} value={0} hideSteppers={true} label={"Roof Area (m^2)"} onKeyUp={()=>{
-                                    // console.log(parseInt(numberRef.current.value))
-                                    setInput(parseInt(numberRef.current.value))
-                                }} ></NumberInput>
-
-                            </Form>
-                            {input}
-                            
+                            <Grid>
+                                <Column lg={4}>
+                                    <Row>
+                                        <h3>Input Parameters:</h3>
+                                        </Row>
+                                    <Row>
+                                        <FormGroup>
+                                            <NumberInput
+                                                ref={numberRef}
+                                                id={"jhgfjhg"}
+                                                value={0}
+                                                hideSteppers={true}
+                                                label={"Roof Area (m^2)"}
+                                                onKeyUp={() => {
+                                                    // console.log(parseInt(numberRef.current.value))
+                                                    setArea(
+                                                        parseInt(
+                                                            numberRef.current
+                                                                .value
+                                                        )
+                                                    );
+                                                }}
+                                            ></NumberInput>
+                                            <NumberInput
+                                                ref={pctRef}
+                                                id={"jhgfjhga"}
+                                                value={85}
+                                                hideSteppers={true}
+                                                label={"Percentage Coverage"}
+                                                onKeyUp={() => {
+                                                    // console.log(parseInt(numberRef.current.value))
+                                                    setArea(
+                                                        parseInt(
+                                                            pctRef.current
+                                                                .value
+                                                        )
+                                                    );
+                                                }}
+                                            ></NumberInput>
+                                        </FormGroup>
+                                    </Row>
+                                    <Row>
+                                        <h3>
+                                            Results:
+                                        </h3>
+                                    </Row>
+                                    <Row>
+                                        <StructuredListWrapper>
+                                            <StructuredListHead>
+                                                <StructuredListRow head>
+                                                    <StructuredListCell head>
+                                                        ColumnA
+                                                    </StructuredListCell>
+                                                    <StructuredListCell head>
+                                                        ColumnB
+                                                    </StructuredListCell>
+                                                    
+                                                </StructuredListRow>
+                                            </StructuredListHead>
+                                            <StructuredListBody>
+                                                <StructuredListRow>
+                                                    <StructuredListCell noWrap>
+                                                        Row 1
+                                                    </StructuredListCell>
+                                                    <StructuredListCell>
+                                                        Row 1
+                                                    </StructuredListCell>
+                                                    
+                                                </StructuredListRow>
+                                                <StructuredListRow>
+                                                    <StructuredListCell noWrap>
+                                                        Row 2
+                                                    </StructuredListCell>
+                                                    <StructuredListCell>
+                                                        Row 2
+                                                    </StructuredListCell>
+                                                    
+                                                </StructuredListRow>
+                                            </StructuredListBody>
+                                        </StructuredListWrapper>
+                                    </Row>
+                                </Column>
+                            </Grid>
                         </Row>
                     </Column>
                 </Grid>
             </Content>
 
-            <GTF coord = {coord}></GTF>
         </main>
     );
 }
